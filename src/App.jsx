77d0292d426025b50a06dc16950b3e52f0a8367e
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-10-23 12:03:37 -0300

    Add Home route

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-    </>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
