473742f9e99935013db3a69be201303eb7276acd
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-10-23 12:02:20 -0300

    Add Open Sans to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      sans: ['"Poppins"', "sans-serif"]
+      sans: ['"Poppins"', "sans-serif"],
+      text: ['"Open Sans"', "sans-serif"]
     },
     extend: {}
   },
