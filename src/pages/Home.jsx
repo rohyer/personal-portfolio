930e44f1bf6d556ed77dd371bdf2db64d5946c67
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-10-24 12:05:37 -0300

    Add more sections components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,15 @@
 import Banner from "../sections/Banner";
+import Contact from "../sections/Contact";
+import Portfolio from "../sections/Portfolio";
 
 const Home = () => {
-  return <Banner />;
+  return (
+    <>
+      <Banner />
+      <Contact />
+      <Portfolio />
+    </>
+  );
 };
 
 export default Home;
