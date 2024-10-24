e56b542647541d4b7bbad12601aef865cd95887d
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-10-24 12:06:15 -0300

    Develop Portfolio section

---
+++ src/sections/Portfolio.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+import { FaArrowRight } from "react-icons/fa";
+import portfolioImg from "../assets/img/portfolio-img.png";
+
+const Portfolio = () => {
+  return (
+    <section className="bg-[#333646]">
+      <div className="flex flex-col lg:container lg:mx-auto xl:px-64 lg:px-32 px-8 py-32">
+        <h2 className="font-montserrat text-[#70717c] font-semibold tracking-wider mb-2">
+          - Portfólio
+        </h2>
+
+        <div className="columns-2 gap-x-20">
+          <div className="w-full mb-24">
+            <p className="font-sans text-white text-2xl mb-8">
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+            </p>
+
+            <p className="font-text text-[#70717c] font-semibold mb-16">
+              Lorem ipsum dolor sit amet consectetur adipisicing.
+            </p>
+
+            <Link
+              className="flex items-center font-montserrat font-semibold text-lg text-yellow-500"
+              to="/portfolio"
+            >
+              Veja mais <FaArrowRight className="ml-4" />
+            </Link>
+          </div>
+
+          <div className="w-full mb-20">
+            <img className="w-full h-auto" src={portfolioImg} alt="" />
+          </div>
+          <div className="w-full mb-20">
+            <img className="w-full h-auto" src={portfolioImg} alt="" />
+          </div>
+          <div className="w-full mb-20">
+            <img className="w-full h-auto" src={portfolioImg} alt="" />
+          </div>
+          <div className="w-full">
+            <img className="w-full h-auto" src={portfolioImg} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
