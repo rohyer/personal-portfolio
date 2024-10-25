a8edb6bb59cf945d58691c4e9340f3d652118fa1
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-10-25 14:43:20 -0300

    Develop career component

--- src/sections/Career.jsx
+++ src/sections/Career.jsx
@@ -8,7 +8,7 @@ const Career = () => {
           - Carreira
         </h2>
 
-        <div className="relative before:content[''] before:absolute before:w-[2px] before:h-[100%] before:bg-gray-300">
+        <div className="relative before:content[''] before:absolute before:w-[2px] before:h-[100%] before:bg-[#70717c]">
           <div className="mb-12 ml-6">
             <h4 className="font-sans text-white text-2xl mb-2">
               Desenvolvedor Web
@@ -16,7 +16,7 @@ const Career = () => {
             <p className="relative before:content[''] before:absolute before:left-[-28px] before:top-0 before:bottom-0 before:m-auto before:w-[11px] before:h-[11px] before:bg-yellow-500 before:rounded-full text-yellow-500">
               Outubro de 2020 - Atual
             </p>
-            <p className="font-sans text-white text-xl mb-2">
+            <p className="font-sans font-medium text-[#70717c] text-xl mb-2">
               Interativa Digital - Remoto
             </p>
             <ul className="text-white">
@@ -70,49 +70,27 @@ const Career = () => {
             <p className="relative before:content[''] before:absolute before:left-[-28px] before:top-0 before:bottom-0 before:m-auto before:w-[11px] before:h-[11px] before:bg-yellow-500 before:rounded-full text-yellow-500">
               Agosto de 2017 - Junho de 2018
             </p>
-            <p className="font-sans text-white text-xl mb-2">
-              Interativa Digital - Remoto
+            <p className="font-sans font-medium text-[#70717c] text-xl mb-2">
+              NIDI Tecnologia
             </p>
             <ul className="text-white">
               <li className="flex items-baseline">
                 <MdKeyboardArrowRight />
-                Desenvolvimento de sistemas: responsável pelo desenvolvimento
-                front-end usando Javascript, HTML, CSS, SASS e Gulp;
+                Desenvolvimento de lojas virtuais: responsável pelo
+                desenvolvimento usando HTML, CSS, SASS e Javascript nas
+                plataformas e-commerce Tray, Xtech e Loja Integrada;
               </li>
 
               <li className="flex items-baseline">
                 <MdKeyboardArrowRight />
-                Manutenção de sistemas: encarregado de corrigir sistemas
-                desenvolvidos em JavaScript, React, Angular e Vue.js;
+                Manutenção de e-commerces: Ajustes e melhorias de desempenho e
+                design de lojas virtuais;
               </li>
 
               <li className="flex items-baseline">
                 <MdKeyboardArrowRight />
                 Desenvolvimento de sites: responsável pelo desenvolvimento
-                front-end usando JavaScript, HTML, CSS, SASS;
-              </li>
-
-              <li className="flex items-baseline">
-                <MdKeyboardArrowRight />
-                Manutenção de sites: responsável por correções e ajustes usando
-                JavaScript, HTML, CSS, SASS;
-              </li>
-
-              <li className="flex items-baseline">
-                <MdKeyboardArrowRight />
-                Implementação de melhorias de SEO: responsável por correções
-                focadas em melhoria no desempenho de busca do Google;
-              </li>
-
-              <li className="flex items-baseline">
-                <MdKeyboardArrowRight />
-                Deploy de sites em ambiente de produção;
-              </li>
-
-              <li className="flex items-baseline">
-                <MdKeyboardArrowRight />
-                Reuniões: participação em reuniões semanais e diárias utilizando
-                a metodologia ágil Kanban.
+                front-end usando JavaScript, HTML, CSS e SASS.
               </li>
             </ul>
           </div>
