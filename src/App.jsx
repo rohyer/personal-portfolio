ba569ab11c271eb36422c71c1333dba20d17c1cc
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-10-29 09:15:05 -0300

    Create refs and pass as prop to Header and Home components

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,35 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
+import { useEffect, useRef } from "react";
 
 function App() {
+  const refLink1 = useRef(null);
+  const refLink2 = useRef(null);
+  const refLink3 = useRef(null);
+  const refLink4 = useRef(null);
+
   return (
     <BrowserRouter>
-      <Header />
+      <Header
+        refLink1={refLink1}
+        refLink2={refLink2}
+        refLink3={refLink3}
+        refLink4={refLink4}
+      />
 
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <Home
+              refAnchor1={refLink1}
+              refAnchor2={refLink2}
+              refAnchor3={refLink3}
+              refAnchor4={refLink4}
+            />
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
