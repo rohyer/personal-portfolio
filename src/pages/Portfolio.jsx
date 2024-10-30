56e18196ccab758640bcef8b2bd56e3699211ef5
Guilherme R. de Oliveira <guilhermerl.dev@gmail.com>
2024-10-30 18:16:14 -0300

    Finish Portfolio page and section

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,29 +1,24 @@
 import { Fancybox } from "@fancyapps/ui";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 import Project from "../components/Project";
-import portfolioMainImg1 from "../assets/img/high-quality/portfolio-main-img-1.png";
-import portfolioMainImg2 from "../assets/img/high-quality/portfolio-main-img-2.png";
-import portfolioMainImg3 from "../assets/img/high-quality/portfolio-main-img-3.png";
-import portfolioMainImg4 from "../assets/img/high-quality/portfolio-main-img-4.png";
-import portfolioMainImg5 from "../assets/img/high-quality/portfolio-main-img-5.png";
-import portfolioMainImg6 from "../assets/img/high-quality/portfolio-main-img-6.png";
-import portfolioMainImg7 from "../assets/img/high-quality/portfolio-main-img-7.png";
-import portfolioMainImg8 from "../assets/img/high-quality/portfolio-main-img-8.png";
-import portfolioMainImg9 from "../assets/img/high-quality/portfolio-main-img-9.png";
-import portfolioMainImg10 from "../assets/img/high-quality/portfolio-main-img-10.png";
-import portfolioMainImg11 from "../assets/img/high-quality/portfolio-main-img-11.png";
-import portfolioMainImg12 from "../assets/img/high-quality/portfolio-main-img-12.png";
-import portfolioMainImg13 from "../assets/img/high-quality/portfolio-main-img-13.png";
-import portfolioMainImg14 from "../assets/img/high-quality/portfolio-main-img-14.png";
-import portfolioMainImg15 from "../assets/img/high-quality/portfolio-main-img-15.png";
-import portfolioMainImg16 from "../assets/img/high-quality/portfolio-main-img-16.png";
-import portfolioMainImg17 from "../assets/img/high-quality/portfolio-main-img-17.png";
-import portfolioMainImg18 from "../assets/img/high-quality/portfolio-main-img-18.png";
+import portfolioMainImg1 from "../assets/img/just-page-portfolio/portfolio-main-img-1.png";
+import portfolioMainImg2 from "../assets/img/just-page-portfolio/portfolio-main-img-2.png";
+import portfolioMainImg3 from "../assets/img/just-page-portfolio/portfolio-main-img-3.png";
+import portfolioMainImg4 from "../assets/img/just-page-portfolio/portfolio-main-img-4.png";
+import portfolioMainImg5 from "../assets/img/just-page-portfolio/portfolio-main-img-5.png";
+import portfolioMainImg6 from "../assets/img/just-page-portfolio/portfolio-main-img-6.png";
+import portfolioMainImg7 from "../assets/img/just-page-portfolio/portfolio-main-img-7.png";
+import portfolioMainImg8 from "../assets/img/just-page-portfolio/portfolio-main-img-8.png";
+import portfolioMainImg9 from "../assets/img/just-page-portfolio/portfolio-main-img-9.png";
+import portfolioMainImg11 from "../assets/img/just-page-portfolio/portfolio-main-img-11.png";
+import portfolioMainImg14 from "../assets/img/just-page-portfolio/portfolio-main-img-14.png";
+import portfolioMainImg15 from "../assets/img/just-page-portfolio/portfolio-main-img-15.png";
+import portfolioMainImg16 from "../assets/img/just-page-portfolio/portfolio-main-img-16.png";
+import portfolioMainImg22 from "../assets/img/just-page-portfolio/portfolio-main-img-22.png";
+import portfolioMainImg24 from "../assets/img/just-page-portfolio/portfolio-main-img-24.png";
 
 const Portfolio = () => {
-  Fancybox.bind("[data-fancybox]", {
-    // Your custom options
-  });
+  Fancybox.bind("[data-fancybox]", {});
 
   return (
     <div className="flex flex-col lg:container lg:mx-auto xl:px-32 2xl:px-64 lg:px-32 px-8 py-32">
@@ -35,104 +30,29 @@ const Portfolio = () => {
         <Project
           title="Barbosa Bezerra Lima Advocacia."
           img={portfolioMainImg1}
-          imgX="20"
-          imgY="50"
         />
-        <Project
-          title="Novo Notícias."
-          img={portfolioMainImg2}
-          imgX="55"
-          imgY="85"
-        />
+        <Project title="Novo Notícias." img={portfolioMainImg2} />
         <Project title="CAURN." img={portfolioMainImg3} imgX="35" imgY="65" />
 
-        <Project
-          title="Hapvendas."
-          img={portfolioMainImg4}
-          imgX="27"
-          imgY="55"
-        />
+        <Project title="Hapvendas." img={portfolioMainImg4} />
         <Project title="Sinmed." img={portfolioMainImg5} imgX="85" imgY="115" />
-        <Project
-          title="Elite Consultores."
-          img={portfolioMainImg6}
-          imgX="40"
-          imgY="70"
-        />
-        <Project
-          title="Chalés Litoral Nordeste."
-          img={portfolioMainImg7}
-          imgX="30"
-          imgY="65"
-        />
-        <Project
-          title="Foco Connect Imobiliária."
-          img={portfolioMainImg8}
-          imgX="30"
-          imgY="65"
-        />
-        <Project
-          title="Gastrocentro."
-          img={portfolioMainImg9}
-          imgX="40"
-          imgY="75"
-        />
-        <Project
-          title="Grupo Mendonça Barros."
-          img={portfolioMainImg10}
-          imgX="15"
-          imgY="50"
-        />
+        <Project title="Elite Consultores." img={portfolioMainImg6} />
+        <Project title="Chalés Litoral Nordeste." img={portfolioMainImg7} />
+        <Project title="Foco Connect Imobiliária." img={portfolioMainImg8} />
+        <Project title="Gastrocentro." img={portfolioMainImg9} />
         <Project
           title="Idealy Corretora de Seguros."
           img={portfolioMainImg11}
-          imgX="25"
-          imgY="60"
         />
-        <Project
-          title="Instituto de Urologia Natal."
-          img={portfolioMainImg12}
-          imgX="25"
-          imgY="60"
-        />
+        <Project title="Multimedical." img={portfolioMainImg14} />
+        <Project title="Pedro Wanderley Advocacia." img={portfolioMainImg15} />
 
+        <Project title="PRM Engenharia." img={portfolioMainImg16} />
         <Project
-          title="Lázaro Imóveis."
-          img={portfolioMainImg13}
-          imgX="30"
-          imgY="60"
+          title="AMARN Associação dos Magistrados do RN."
+          img={portfolioMainImg22}
         />
-        <Project
-          title="Multimedical."
-          img={portfolioMainImg14}
-          imgX="20"
-          imgY="50"
-        />
-        <Project
-          title="Pedro Wanderley Advocacia."
-          img={portfolioMainImg15}
-          imgX="40"
-          imgY="75"
-        />
-
-        <Project
-          title="PRM Engenharia."
-          img={portfolioMainImg16}
-          imgX="30"
-          imgY="65"
-        />
-        <Project
-          title="Reforest And Recycle For a Future."
-          img={portfolioMainImg17}
-          imgX="45"
-          imgY="75"
-        />
-        <Project
-          title="Shirley Ferreira Imóveis."
-          img={portfolioMainImg18}
-          imgX="50"
-          imgY="85"
-        />
+        <Project title="Atual Viagens." img={portfolioMainImg24} />
       </div>
     </div>
   );
--- src/sections/Portfolio.jsx
+++ src/sections/Portfolio.jsx
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 import { FaArrowRight } from "react-icons/fa";
-import portfolioMainImg2 from "../assets/img/high-quality/portfolio-main-img-2.png";
-import portfolioMainImg1 from "../assets/img/high-quality/portfolio-main-img-1.png";
-import portfolioMainImg3 from "../assets/img/high-quality/portfolio-main-img-3.png";
-import portfolioMainImg4 from "../assets/img/high-quality/portfolio-main-img-4.png";
-import portfolioMainImg5 from "../assets/img/high-quality/portfolio-main-img-5.png";
-import portfolioMainImg9 from "../assets/img/high-quality/portfolio-main-img-9.png";
+import Project from "../components/Project";
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
+import portfolioMainImg1 from "../assets/img/just-page-portfolio/portfolio-main-img-1.png";
+import portfolioMainImg3 from "../assets/img/just-page-portfolio/portfolio-main-img-3.png";
+import portfolioMainImg2 from "../assets/img/just-page-portfolio/portfolio-main-img-2.png";
+import portfolioMainImg5 from "../assets/img/just-page-portfolio/portfolio-main-img-5.png";
 
 const Portfolio = ({ refAnchor2 }) => {
+  Fancybox.bind("[data-fancybox]", {});
+
   return (
     <section className="relative bg-[#333646]">
       <div id="portfolio" className="absolute top-[-6rem]"></div>
@@ -39,106 +42,14 @@ const Portfolio = ({ refAnchor2 }) => {
             </Link>
           </div>
 
-          <div className="relative group w-full max-h-[500px] overflow-hidden bg-[#242734] mb-20">
-            <div className="flex flex-col absolute left-0 top-0 w-full h-full z-10 px-8 py-8 opacity-0 transition-all group-hover:opacity-100 bg-[#000000b3]">
-              <p className="font-montserrat text-xl font-semibold text-white">
-                Barbosa Bezerra Lima Advocacia.
-              </p>
-              <p className="font-montserrat text-md font-semibold text-white mb-4">
-                Responsável pelo desenvolvimento.
-              </p>
-              <p className="font-montserrat text-sm text-white">Descrição: </p>
-            </div>
-            <img
-              className="w-full h-auto md:scale-[1.1] md:rotate-[-5deg] md:translate-x-[40px] md:translate-y-[170px]"
-              src={portfolioMainImg1}
-              alt=""
-            />
-          </div>
-
-          <div className="relative group w-full max-h-[500px] overflow-hidden bg-[#242734] mb-20">
-            <div className="flex flex-col absolute left-0 top-0 w-full h-full z-10 px-8 py-8 opacity-0 transition-all group-hover:opacity-100 bg-[#000000b3]">
-              <p className="font-montserrat text-xl font-semibold text-white">
-                Sinmed RN.
-              </p>
-              <p className="font-montserrat text-md font-semibold text-white mb-4">
-                Responsável pelo desenvolvimento.
-              </p>
-              <p className="font-montserrat text-sm text-white">Descrição: </p>
-            </div>
-            <img
-              className="w-full h-auto md:scale-[1.1] md:rotate-[-5deg] md:translate-x-[135px] md:translate-y-[275px]"
-              src={portfolioMainImg5}
-              alt=""
-            />
-          </div>
-
-          <div className="relative group w-full max-h-[500px] overflow-hidden bg-[#242734] mb-20">
-            <div className="flex flex-col absolute left-0 top-0 w-full h-full z-10 px-8 py-8 opacity-0 transition-all group-hover:opacity-100 bg-[#000000b3]">
-              <p className="font-montserrat text-xl font-semibold text-white">
-                Hapvendas.
-              </p>
-              <p className="font-montserrat text-md font-semibold text-white mb-4">
-                Responsável pelo desenvolvimento.
-              </p>
-              <p className="font-montserrat text-sm text-white">Descrição: </p>
-            </div>
-            <img
-              className="w-full h-auto md:scale-[1.1] md:rotate-[-5deg] md:translate-x-[45px] md:translate-y-[170px]"
-              src={portfolioMainImg4}
-              alt=""
-            />
-          </div>
-          <div className="relative group w-full max-h-[500px] overflow-hidden bg-[#242734] mb-20">
-            <div className="flex flex-col absolute left-0 top-0 w-full h-full z-10 px-8 py-8 opacity-0 transition-all group-hover:opacity-100 bg-[#000000b3]">
-              <p className="font-montserrat text-xl font-semibold text-white">
-                CAURN.
-              </p>
-              <p className="font-montserrat text-md font-semibold text-white mb-4">
-                Responsável pelo desenvolvimento.
-              </p>
-              <p className="font-montserrat text-sm text-white">Descrição: </p>
-            </div>
-            <img
-              className="w-full h-auto md:scale-[1.1] md:rotate-[-5deg] md:translate-x-[55px] md:translate-y-[180px]"
-              src={portfolioMainImg3}
-              alt=""
-            />
-          </div>
-
-          <div className="relative group w-full max-h-[500px] overflow-hidden bg-[#242734] mb-20">
-            <div className="flex flex-col absolute left-0 top-0 w-full h-full z-10 px-8 py-8 opacity-0 transition-all group-hover:opacity-100 bg-[#000000b3]">
-              <p className="font-montserrat text-xl font-semibold text-white">
-                Novo Notícias.
-              </p>
-              <p className="font-montserrat text-md font-semibold text-white mb-4">
-                Responsável pelo desenvolvimento.
-              </p>
-              <p className="font-montserrat text-sm text-white">Descrição: </p>
-            </div>
-            <img
-              className="w-full h-auto md:scale-[1.1] md:rotate-[-5deg] md:translate-x-[95px] md:translate-y-[220px]"
-              src={portfolioMainImg2}
-              alt=""
-            />
-          </div>
-
-          <div className="relative group w-full max-h-[500px] overflow-hidden bg-[#242734] mb-20">
-            <div className="flex flex-col absolute left-0 top-0 w-full h-full z-10 px-8 py-8 opacity-0 transition-all group-hover:opacity-100 bg-[#000000b3]">
-              <p className="font-montserrat text-xl font-semibold text-white">
-                Gastrocentro.
-              </p>
-              <p className="font-montserrat text-md font-semibold text-white mb-4">
-                Responsável pelo desenvolvimento.
-              </p>
-              <p className="font-montserrat text-sm text-white">Descrição: </p>
-            </div>
-            <img
-              className="w-full h-auto md:scale-[1.1] md:rotate-[-5deg] md:translate-x-[75px] md:translate-y-[215px]"
-              src={portfolioMainImg9}
-              alt=""
-            />
-          </div>
+          <Project
+            title="Barbosa Bezerra Lima Advocacia."
+            img={portfolioMainImg1}
+            page="home"
+          />
+          <Project title="NOVO Notícias." img={portfolioMainImg2} page="home" />
+          <Project title="CAURN." img={portfolioMainImg3} page="home" />
+          <Project title="Sinmed RN." img={portfolioMainImg5} page="home" />
         </div>
       </div>
     </section>
